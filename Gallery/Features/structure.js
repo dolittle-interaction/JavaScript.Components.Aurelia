d14f080f1e9f69674cf47793126bfb641d99d471
Einar Ingebrigtsen <einar@dolittle.com>
2019-03-13 19:39:09 +0100

    Changing name -> navigation view

--- Gallery/Features/structure.js
+++ Gallery/Features/structure.js
@@ -1,14 +1,14 @@
 export default {
     components: [
         {
-            name:'Side bar',
-            description:'A sidebar for either navigational purposes or just hold information',
-            component:'sideBar'
+            name: 'Navigation View',
+            description: 'A view holding navigation items',
+            component: 'navigationView'
         },
         {
-            name:'Pivot',
-            description:'Pivot can be used for navigtational purposes on a page, typically subb navigation. A good use case could be for navigating different filters',
-            component:'pivot'
+            name: 'Pivot',
+            description: 'Pivot can be used for navigtational purposes on a page, typically subb navigation. A good use case could be for navigating different filters',
+            component: 'pivot'
         }
 
     ]
