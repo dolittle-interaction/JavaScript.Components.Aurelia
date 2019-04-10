d337b3249098027e778e2eabe2a739ce027e4470
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-10 16:27:05 +0200

    Single quotes

--- Source/Templating/parts.js
+++ Source/Templating/parts.js
@@ -2,7 +2,7 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { processContent } from "aurelia-framework";
+import { processContent } from 'aurelia-framework';
 
 function handlePart(name, element) {
     let partContainer = element.querySelector(name);
