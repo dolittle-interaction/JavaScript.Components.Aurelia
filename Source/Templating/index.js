45d3a03dfec582a627865ba3d49252b73254adbb
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-11 15:36:04 +0200

    Exporting scope

---
+++ Source/Templating/index.js
@@ -0,0 +1,12 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+export * from './hasParts';
+export * from './HierarchyRepeat';
+export * from './HierarchyRepeatChildren';
+export * from './HierarchyRepeater';
+export * from './HierarchyRepeaterItem';
+export * from './MissingHierarchyChildrenProperty';
+export * from './MissingParentHierarchyRepeat';
+export * from './TooManyElementsWithHierarchyChildrenProperty';
