718235414df8cb88c38c57c6b5fab6d807779f7f
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-11 10:59:25 +0200

    JSDoc

--- Source/Templating/HierarchyRepeaterItem.js
+++ Source/Templating/HierarchyRepeaterItem.js
@@ -15,18 +15,28 @@ export class HierarchyRepeaterItem {
     hierarchyLevel = 0;
 
     /**
+     * Gets or sets the parent for the item
+     */
+    parent = null;
+
+    /**
      * Gets wether or not the item is considered to be at the root level of the hierarchy or not
      */
     get isRootLevel() {
         return isRootLevel === 0;
     }
 
+    /**
+     * Gets the children for the item
+     */
     get children() {
         return this.#children;
     }
 
-    parent = null;
-
+    /**
+     * Adds a child to the item
+     * @param {HierarchyRepeaterItem} child Child to add
+     */
     addChild(child) {
         this.#children.push(child);
     }
