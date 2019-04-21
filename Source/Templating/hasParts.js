01232c37aa9de20f9f726b3c9bea3855cce84939
Einar Ingebrigtsen <einar@dolittle.com>
2019-04-21 10:33:28 +0200

    Changing to using replaceable parts

--- Source/Templating/hasParts.js
+++ Source/Templating/hasParts.js
@@ -2,31 +2,41 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { processContent } from 'aurelia-framework';
+import { processContent, FEATURE } from 'aurelia-framework';
 
 function handlePart(name, element) {
     let partContainer = element.querySelector(name);
-
     if (partContainer) {
-        while (partContainer.children.length > 0) {
-            partContainer.firstElementChild.setAttribute('slot', name);
-            element.append(partContainer.firstElementChild);
+        const template = document.createElement('template');
+        FEATURE.ensureHTMLTemplateElement(template);
+        template.setAttribute('replace-part', name);
+        let child;
+        while (child = partContainer.firstChild) {
+            partContainer.removeChild(child);
+            template.content.appendChild(child);
         }
+        element.appendChild(template);
     }
 }
 
 /**
- * Decorator used on components that will have parts mapped by convention into their matching slots
- * 
- * This works by looking at named slots and finding elements with the same name and matching these
+ * Decorator used on components that will have parts mapped by convention into their matching replacable part
+ *
+ * This works by looking at named replaceable parts and finding elements with the same name and matching these
  * inside the use of the custom element.
  */
 export function hasParts() {
     function contentProcessor(viewCompiler, viewResources, element, parentInstruction) {
-        for (let instructionName in parentInstruction.type.viewFactory.instructions) {
-            let instruction = parentInstruction.type.viewFactory.instructions[instructionName];
-            if (instruction.slotName) handlePart(instruction.slotName, element);
-        }
+        let partTemplates = parentInstruction.type.viewFactory.template.querySelectorAll('[part]');
+        partTemplates.forEach(partTemplate => {
+            for (let attributeIndex = 0; attributeIndex < partTemplate.attributes.length; attributeIndex++) {
+                let attribute = partTemplate.attributes[attributeIndex];
+                if (attribute.localName === 'part') {
+                    handlePart(attribute.value, element);
+                    break;
+                }
+            }
+        });
         return true;
     }
     return processContent(contentProcessor);
