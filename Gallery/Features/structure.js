b78dd43f4b6abab1a795b2b5a8e4b0bb217ae0a1
Vidar Aune Westrum <vidar@dolittle.com>
2019-05-23 10:02:53 +0200

    added missing license headers

--- Gallery/Features/structure.js
+++ Gallery/Features/structure.js
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 export default {
     components: [
         {
@@ -14,9 +18,9 @@ export default {
         },
         {
             name: 'Pivot',
-            description: 'Pivot can be used for navigtational purposes on a page, typically subb navigation. A good use case could be for navigating different filters',
+            description:
+                'Pivot can be used for navigtational purposes on a page, typically subb navigation. A good use case could be for navigating different filters',
             component: 'pivot'
         }
-
     ]
-}
+};
