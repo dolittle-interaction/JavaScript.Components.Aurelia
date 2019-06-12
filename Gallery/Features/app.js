144cfe81f7761adacbe67d866c8ac595ca7db62b
Vidar Aune Westrum <vidar@dolittle.com>
2019-06-12 15:58:51 +0200

    Added missing license headers

--- Gallery/Features/app.js
+++ Gallery/Features/app.js
@@ -1,15 +1,16 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import { PLATFORM } from 'aurelia-pal';
 import style from '../styles/style.scss';
 
 export class app {
-    constructor() {
-    }
+    constructor() {}
 
     configureRouter(config, router) {
         config.options.pushState = true;
-        config.map([
-            { route: ['', 'components'], name: 'Index', moduleId: PLATFORM.moduleName('index') }
-        ]);
+        config.map([{ route: ['', 'components'], name: 'Index', moduleId: PLATFORM.moduleName('index') }]);
 
         this.router = router;
     }
