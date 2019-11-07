af3eaa245de0ea4bd19ed3fd582f902ac912cce6
ebe <ebenezer@dolittle.com>
2019-11-07 10:00:42 +0100

    create components

--- Source/ExpandablePane/ExpandablePane.ts
+++ Source/ExpandablePane/ExpandablePane.ts
@@ -3,12 +3,13 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { hasParts } from '../Templating/hasParts';
 import { bindable } from 'aurelia-framework';
 
 export class ExpandablePane {
-  @bindable isExpanded = false;
+  @bindable isExpanded: boolean=false;
 
   toggleExpansion(){
-      this.isExpanded = !this.isExpanded;
+    this.isExpanded=!this.isExpanded;
   }
 }
