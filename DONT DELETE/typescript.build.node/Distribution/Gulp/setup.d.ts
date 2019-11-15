f4cc8a0fbe27dc136650ac90948869cc8c09354d
Sindre Wilting <sindrewilting@gmail.com>
2019-11-15 20:23:33 +0100

    Delete the Don't delete

--- DONT DELETE/typescript.build.node/Distribution/Gulp/setup.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Setup the tasks from this package
- * @param {any} originalExports The original exports object in the scope of the gulpfile importing this
- */
-export default function setupGulp(originalExports: any): void;
