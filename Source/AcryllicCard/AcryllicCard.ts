a385e5ddb7670ac480c478703d0caa2fedefb957
Vidar Aune Westrum <vidar@dolittle.com>
2019-11-19 10:54:42 +0100

    introduced acryllic-card component

---
+++ Source/AcryllicCard/AcryllicCard.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Dolittle. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+import {containerless} from 'aurelia-framework';
+
+@containerless()
+export class Card {
+}
