5ed8c36b5ad36004d269d19be5d615e3b63facdd
Vidar Aune Westrum <vidar@dolittle.com>
2019-12-09 23:21:01 +0100

    Set the behavior of the Acrylic card

--- Source/AcrylicCard/AcrylicCard.ts
+++ Source/AcrylicCard/AcrylicCard.ts
@@ -2,12 +2,24 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import { bindable, inject } from 'aurelia-framework';
 
-import { hasParts } from '../Templating/hasParts';
-import { customElement, bindable } from 'aurelia-framework';
-
-@customElement('acrylic-card')
-@hasParts()
+@inject(Element)
 export class AcrylicCard {
-  @bindable width: Number = 22;
+  @bindable width: Number = 0;
+  private uniqueCardCssClass: string = '';
+  private customCardStyle: string = '';
+  constructor(element: any) {
+    console.log(element);
+    this.uniqueCardCssClass = 'card-' + element.getAttribute('au-target-id');
+  }
+  attached() {
+    this.populateCustomWidthStyleTag();
+  }
+
+  private populateCustomWidthStyleTag() {
+    if (this.width > 0) {
+      this.customCardStyle = `<style>.${this.uniqueCardCssClass}{width:${this.width}rem;}</style>`;
+    }
+  }
 }
