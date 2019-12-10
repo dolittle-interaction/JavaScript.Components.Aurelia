d7d4db2f6267296c1d9182029de313028e31a5f7
Vidar Aune Westrum <vidar@dolittle.com>
2019-12-10 13:41:47 +0100

    Fixed test on navigationViewItem

--- Source/NavigationView/for_NavigationViewItem/when_toggling_expansion.ts
+++ Source/NavigationView/for_NavigationViewItem/when_toggling_expansion.ts
@@ -3,24 +3,39 @@
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 import { NavigationViewItem } from '../NavigationViewItem';
+let item: NavigationViewItem;
+describe('when toggling expansion and is collapsed and not having children', () => {
+  beforeEach(() => {
+    item = new NavigationViewItem();
+    item.toggleExpansion();
+  });
+  it('should not be expanded', () => item.isExpanded.should.be.false);
+});
 
-describe('when toggling expansion', () => {
-    describe('and it is collapsed', () => {
-        describe('and it has children', () => {
-            let item = new NavigationViewItem();
-            item.isExpanded = false;
-            item.toggleExpansion();
-            item.addChild(new NavigationViewItem());
-            console.log(item);
-            it('should be expanded', () => item.isExpanded.should.be.true);
-        });
-    });
+describe('when toggling expansion and is collapsed and having children', () => {
+  beforeEach(() => {
+    item = new NavigationViewItem();
+    item.addChild(new NavigationViewItem());
+    item.toggleExpansion();
+  });
+  it('should be expanded', () => item.isExpanded.should.be.true);
+});
 
-    describe('and it is expanded', () => {
-        let item = new NavigationViewItem();
-        item.isExpanded = true;
-        item.toggleExpansion();
+describe('when toggling expansion and is expanded and not having children', () => {
+  beforeEach(() => {
+    item = new NavigationViewItem();
+    item.isExpanded = true;
+    item.toggleExpansion();
+  });
+  it('should not be expanded', () => item.isExpanded.should.be.false);
+});
 
-        it('should not be expanded', () => item.isExpanded.should.be.false);
-    });
+describe('when toggling expansion and is expanded and having children', () => {
+  beforeEach(() => {
+    item = new NavigationViewItem();
+    item.isExpanded = true;
+    item.addChild(new NavigationViewItem());
+    item.toggleExpansion();
+  });
+  it('should not be expanded', () => item.isExpanded.should.be.false);
 });
