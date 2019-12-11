068fa716c3aa4233d643012b50e738334325f123
Vidar Aune Westrum <vidar@dolittle.com>
2019-12-11 10:59:07 +0100

    Make tests run

--- Source/ExpandableCard/for_ExpandableCard/when_toggling_a_collapsed_card.ts
+++ Source/ExpandableCard/for_ExpandableCard/when_toggling_a_collapsed_card.ts
@@ -2,6 +2,7 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import "aurelia-polyfills";
 import { ExpandableCard } from '../ExpandableCard';
 
 describe('when toggling a collapsed pane', () => {
--- Source/ExpandableCard/for_ExpandableCard/when_toggling_an_expanded_card.ts
+++ Source/ExpandableCard/for_ExpandableCard/when_toggling_an_expanded_card.ts
@@ -2,16 +2,17 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
+import "aurelia-polyfills";
 import { ExpandableCard } from '../ExpandableCard';
 
 describe('when toggling a collapsed pane', () => {
   let expandableCard: ExpandableCard;
-
+  
   beforeEach(() => {
     expandableCard = new ExpandableCard();
     expandableCard.isExpanded = true;
     expandableCard.toggleExpansion();
   });
-
+  
   it('should be expanded', () => expandableCard.isExpanded.should.be.false);
 });
