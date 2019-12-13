5d1de0a6b50d3a5af3d084cb3537ebd74dc3c9fa
Vidar Aune Westrum <vidar@dolittle.com>
2019-12-13 11:27:41 +0100

    setting visual usage in gallery

--- Gallery/Features/for_something/when_something.ts
+++
@@ -1,7 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Dolittle. All rights reserved.
- *  Licensed under the MIT License. See LICENSE in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-describe('when something', () => {
-    it('should check something', () => true.should.be.true);
-});
