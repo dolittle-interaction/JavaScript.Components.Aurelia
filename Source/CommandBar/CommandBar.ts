ed6b886b9d635758278e339adee7b417261bb66e
Vidar Aune Westrum <vidar@dolittle.com>
2019-12-13 11:28:06 +0100

    Don't need this

--- Source/CommandBar/CommandBar.ts
+++ Source/CommandBar/CommandBar.ts
@@ -2,12 +2,11 @@
  *  Copyright (c) Dolittle. All rights reserved.
  *  Licensed under the MIT License. See LICENSE in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { customElement, bindable, observable } from 'aurelia-framework';
+import { bindable, observable } from 'aurelia-framework';
 import { hasParts } from '../Templating/hasParts';
 import { labelPosition } from './labelPosition';
 import { commandLocation } from './commandLocation';
 
-@customElement('command-bar')
 @hasParts()
 export class CommandBar {
     @bindable commandLocation: any;
