1c7d40b6ccbedc48d6b04564e1ec5eee5a5ba823
Vidar Aune Westrum <vidar@dolittle.com>
2020-01-06 12:53:23 +0100

    StyleCop

--- Source/Controls/CommandBar/commandLocation.ts
+++ Source/Controls/CommandBar/commandLocation.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 const commandLocation = {
-    left: 'left',
-    center: 'center',
-    right: 'right',
-    distributed: 'distributed'
+  left: 'left',
+  center: 'center',
+  right: 'right',
+  distributed: 'distributed'
 };
 
 export { commandLocation };
