7ce163217b397e9a02f62045f7c43b0562e4a92c
Vidar Aune Westrum <vidar@dolittle.com>
2020-01-15 11:30:05 +0100

    These are strings

--- Source/Controls/CommandBar/CommandBarButton.ts
+++ Source/Controls/CommandBar/CommandBarButton.ts
@@ -1,13 +1,11 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import { customElement, containerless, bindable } from 'aurelia-framework';
+import { containerless, bindable } from 'aurelia-framework';
 
-@customElement('command-bar-button')
 @containerless()
 export class CommandBarButton {
-    @bindable icon: any;
-    @bindable label: any;
-    @bindable linkUrl: any;
-    // tslint:disable-next-line: no-empty
-    constructor(){}
+  @bindable label: String = '';
+  @bindable linkUrl: String = '';
+  @bindable iconUrl: String = '';
+  constructor() {}
 }
