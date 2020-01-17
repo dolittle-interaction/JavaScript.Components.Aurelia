5cdde40dfefc914e8aaaa873f327cb0294ae487b
Vidar Aune Westrum <vidar@dolittle.com>
2020-01-17 11:45:17 +0100

    replaced linkUrl with Callback function

--- Source/Controls/CommandBar/CommandBarButton.ts
+++ Source/Controls/CommandBar/CommandBarButton.ts
@@ -1,11 +1,19 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 import { containerless, bindable } from 'aurelia-framework';
+import { Component } from '../../Base/component';
 
 @containerless()
-export class CommandBarButton {
+export class CommandBarButton extends Component {
   @bindable label: String = '';
-  @bindable linkUrl: String = '';
   @bindable iconUrl: String = '';
-  constructor() {}
+  @bindable clicked: any;
+  constructor() {
+    super();
+  }
+  handleClick(parameter: String) {
+    if (this.clicked) {
+      this.clicked(parameter);
+    }
+  }
 }
