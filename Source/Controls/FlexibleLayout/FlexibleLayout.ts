3e47696c8491824464a34ba29ecf9222de8701ac
Vidar Aune Westrum <vidar@dolittle.com>
2020-01-21 12:55:47 +0100

    Update to style layout respectful to it's properties

--- Source/Controls/FlexibleLayout/FlexibleLayout.ts
+++ Source/Controls/FlexibleLayout/FlexibleLayout.ts
@@ -1,20 +1,23 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-import { bindable } from 'aurelia-framework';
-import { horizontalAlign } from './horizontalAlign';
-import { verticalAlign } from './verticalAlign';
-import { flowDirection } from './flowDirection';
-import { Component } from '../../Base/component';
+import { bindable } from "aurelia-framework";
+import { horizontalAlign } from "./horizontalAlign";
+import { verticalAlign } from "./verticalAlign";
+import { flowDirection } from "./flowDirection";
+import { Component } from "../../Base/component";
 
 export class FlexibleLayout extends Component {
-  @bindable horizontalAlign: string;
-  @bindable verticalAlign: string;
-  @bindable flowDirection: string;
+    @bindable horizontalAlign: string;
+    @bindable verticalAlign: string;
+    @bindable flowDirection: string;
+    @bindable columnSpace: number = 0;
+    uniqueLayoutCssClass: string = "";
 
-  constructor() {
-    super();
-    this.horizontalAlign = horizontalAlign.left;
-    this.verticalAlign = verticalAlign.top;
-    this.flowDirection = flowDirection.row;
-  }
+    constructor() {
+        super();
+        this.uniqueLayoutCssClass = "card-" + this.uniqueIdentifier;
+        this.horizontalAlign = horizontalAlign.left;
+        this.verticalAlign = verticalAlign.top;
+        this.flowDirection = flowDirection.row;
+    }
 }
--- Source/Controls/FlexibleLayout/verticalAlign.ts
+++ Source/Controls/FlexibleLayout/verticalAlign.ts
@@ -1,10 +1,10 @@
 // Copyright (c) Dolittle. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 export const verticalAlign = {
-  top: 'flex-start',
-  bottom: 'flex-end',
-  center: 'center',
-  distributed: 'space-around',
-  distributedToEdge: 'space-between',
-  stretch: 'stretch'
+    top: "top",
+    bottom: "bottom",
+    center: "center",
+    distributed: "distributed",
+    distributedEdge: "distributed-edge",
+    stretch: "stretch"
 };
